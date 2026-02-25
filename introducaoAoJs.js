93e4caec4e8aa64de855a12c710a262fe5afc15d
Diogo Bezerra <diogoleitebezerra@gmail.com>
2026-02-25 09:07:59 -0300

    Atualização do arquivo de introdução e inicialização do arquivo de strings/booleanos.

--- introducaoAoJs.js
+++ introducaoAoJs.js
@@ -1,5 +1,10 @@
-// FUNCOES
-
+// FUNCOES | inicio
+// --Comando 1:--
+/*
+    1. Crie um programa que declare o nome de um carro, seu preço de custo (ex: 30000) e o lucro
+    desejado da loja (ex: 5000). Some esses valores para chegar ao preço final de venda. Mostre no
+    console uma frase como: 'O preço final do carro é: 35000'."
+*/
 function declaracao() {
     const car = "Uno de Escada"
     let lucro = 10000, preco = 30000
@@ -12,6 +17,12 @@ function declaracao() {
 //     b = troca
 //     return [a, b]
 // }
+
+// --Comando 2:--
+/*
+    2. Declare duas variáveis, canecaA com o valor "Café" e canecaB com o valor "Leite". Seu objetivo
+    é trocar os conteúdos, de forma que a canecaA termine com "Leite" e a canecaB com "Café".
+*/
 function canecas() {
     let canecaA = "Café"
     let canecaB = "Leite"
@@ -22,6 +33,11 @@ function canecas() {
     return "As Canecas A e B, inicialmente preenchidas com Café e Leite, respectivamente, agora apresentam seus valores invertidos, sendo Caneca a: " + canecaA + " e Caneca b: " + canecaB
 }
 
+// --Comando 3:--
+/*
+    3. Declare três notas de um aluno (ex: 7, 8 e 9). Tente calcular a média somando as três e dividindo por
+    3 na mesma linha.
+*/
 function notas() {
     let aluno = {
         nome: "Diogo",
@@ -34,6 +50,14 @@ function notas() {
     return "O aluno " + aluno.nome + " obteve a média de: " + media
 }
 
+
+// --Comando 4:--
+/*
+    4. Escreva um código que crie e exiba no console o typeof das seguintes operações bizzaras:
+        Uma variável com um texto multiplicada por um número
+        O número 50 dividido por 0
+        Uma variável declarada mas sem valor atribuído
+*/
 function operacoesBizarras() {
     let texto = "que bizarro mano" * 2
     let numero = 50 / 0
@@ -43,8 +67,9 @@ function operacoesBizarras() {
     console.log(typeof numero)
     console.log(typeof naoTemValor)
 }
+// FUNCOES | fim
 
-// CONSOLE LOGS
+// CONSOLE LOGS | inicio
 console.log(declaracao())
 console.log("---------------------------------------------")
 console.log(canecas())
@@ -53,3 +78,4 @@ console.log(notas())
 console.log("---------------------------------------------")
 console.log(operacoesBizarras())
 console.log("---------------------------------------------")
+// CONSOLE LOGS | fim
---
+++ strings&booleanos.js
@@ -0,0 +1,19 @@
+//Funções | Inicio
+
+// --Comando 1:--
+/*
+    1. Crie uma variável com um e-mail bagunçado: ' Joao.SILVA@Gmail.com '. Sua missão é limpar
+    os espaços e transformar tudo para letras minúsculas em uma única linha de código (ou duas).
+    Mostre o resultado final limpo: 'joao.silva@gmail.com'
+*/
+
+ function emailBaguncado() {
+    let email = " diOgo.BezErRa@Gmail.cOm "
+    console.log(email.trim().toLocaleLowerCase())
+ }
+
+
+ // CONSOLE LOGS | Inicio
+ console.log(emailBaguncado())
+
+ // CONSOLE LOGS | Fim
