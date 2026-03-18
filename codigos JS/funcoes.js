4bbadb19120c3fbde2f27ec145b7ca02cf632e0b
Diogo Bezerra <diogoleitebezerra@gmail.com>
2026-03-18 09:17:20 -0300

    Criação do arquivo de funções e declaração dos desafios e funções iniciais.

---
+++ codigos JS/funcoes.js
@@ -0,0 +1,42 @@
+// Resolvendo Desafios
+/*  Comando 1: 
+    --- Validador de Senha --- 
+    
+    Crie uma Arrow Function chamada validarSenha. Ela deve receber uma string e retornar true se a
+    senha tiver 8 ou mais caracteres, e false caso contrário. Use o retorno implícito.
+*/
+
+function validador() {
+    //...
+}
+
+/*  Comando 2: 
+    --- O Contador de Vogais  --- 
+    
+    Crie uma função que recebe uma string. Use um loop para percorrer a string e conte quantas vogais
+    (a, e, i, o, u) ela possui. A função deve retornar o número total de vogais encontradas.
+*/
+
+function contador() {
+    //...
+}
+
+
+/*  Comando 3: 
+    --- Filtro de Funcionários --- 
+    
+    Você tem um array de objetos:
+
+        const equipe = [
+        {nome: "Ana", salario: 3000},
+        {nome: "Beto", salario: 6000},
+        {nome: "Carla", salario: 2500}
+        ]
+
+    Crie uma função que recebe esse array e retorna um novo array contendo apenas os nomes dos
+    funcionários que ganham mais de 4000.
+*/
+
+function filtro() {
+    //...
+}
